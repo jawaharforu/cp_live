0cf8d8fa225efa4bed4afae080cb4098dd6e99e3
jawaharforu <jawaharforu@gmail.com>
2019-04-03 23:06:34 +0530

    new changes

--- resources/views/pages/main.blade.php
+++ resources/views/pages/main.blade.php
@@ -1,4 +1,21 @@
 @extends('layouts.inner')
 @section('content')
-    
+<div class="container">
+        <div class="row justify-content-md-center">
+            <div class="col-md-6">
+                <div><img class="w-100" src="/img/curepeople-mobile_page-0006-copy.jpg" /></div>
+                <div class="p-4">
+                    <div class="card bg-light text-dark">
+                        <button class="btn btn-info whatareyou">What are you looking for ?</button>
+                        <div class="card-body">
+                            <p class="dashboard-text orrangecls font-weight-bold">Live Consultation</p>
+                            <p class="dashboard-text">Virtual Consultation - Free</p>
+                            <p class="dashboard-text">Home Remedies - Free</p>
+                            <p class="dashboard-text mb-5">Preventive Medicine</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
