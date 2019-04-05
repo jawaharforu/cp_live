ad3f722d402d6facbcaaa3f5ec52652a6fa35aa9
Karthik Girraju <karthik.girraj@gmail.com>
2019-04-05 09:42:56 +0530

    code formatted and nothing changed

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,17 @@
 <?php
-Route::get('/', function () {
-    return view('pages.home');
-});
-Route::post('forgot', ['uses' => 'AdminCmsUsersController@postForgot', 'as' => 'postForgot']);
-Route::get('forgot', ['uses' => 'AdminCmsUsersController@getForgot', 'as' => 'getForgot']);
-Route::post('register', ['uses' => 'AdminCmsUsersController@postRegister', 'as' => 'postRegister']);
-Route::get('register', ['uses' => 'AdminCmsUsersController@getRegister', 'as' => 'getRegister']);
-Route::get('logout', ['uses' => 'AdminCmsUsersController@getLogout', 'as' => 'getLogout']);
-Route::post('login', ['uses' => 'AdminCmsUsersController@postLogin', 'as' => 'postLogin']);
-Route::get('login', ['uses' => 'AdminCmsUsersController@getLogin', 'as' => 'getLogin']);
-Route::group( [ 'middleware' => [ 'web', '\App\Http\Middleware\CheckLoggedin' ]], function () {
-    Route::any('dashboard', 'MainController@getDashboard');
-});
+	
+	use Illuminate\Support\Facades\Route;
+	
+	Route::get( '/', function () {
+		return view( 'pages.home' );
+	} );
+	Route::post( 'forgot', [ 'uses' => 'AdminCmsUsersController@postForgot', 'as' => 'postForgot' ] );
+	Route::get( 'forgot', [ 'uses' => 'AdminCmsUsersController@getForgot', 'as' => 'getForgot' ] );
+	Route::post( 'register', [ 'uses' => 'AdminCmsUsersController@postRegister', 'as' => 'postRegister' ] );
+	Route::get( 'register', [ 'uses' => 'AdminCmsUsersController@getRegister', 'as' => 'getRegister' ] );
+	Route::get( 'logout', [ 'uses' => 'AdminCmsUsersController@getLogout', 'as' => 'getLogout' ] );
+	Route::post( 'login', [ 'uses' => 'AdminCmsUsersController@postLogin', 'as' => 'postLogin' ] );
+	Route::get( 'login', [ 'uses' => 'AdminCmsUsersController@getLogin', 'as' => 'getLogin' ] );
+	Route::group( [ 'middleware' => [ 'web', '\App\Http\Middleware\CheckLoggedin' ] ], function () {
+		Route::any( 'dashboard', 'MainController@getDashboard' );
+	} );
